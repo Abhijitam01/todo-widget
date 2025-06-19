0725f3ffe07dec10bdade954762c6aea0d8008cd
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-06-20 01:31:44 +0530

    init the repo

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { app , shell , ipcMain , screen, BrowserWindow } from 'electron' ;
+import * as path from 'path' ;
+
+let mainWindow: BrowserWindow | null = null ;
+
+const createWindow = () : void => {
+    const 
+}
